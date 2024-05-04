17db330da72f086b11363a31f2985b48ac9622ce
ky0he1 <kyouta1218@gmail.com>
2024-05-04 22:37:10 +0900

    Paging Implementation

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,35 @@
 #![test_runner(sros::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use sros::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+entry_point!(kernel_main);
 
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use sros::memory::{self, BootInfoFrameAllocator};
+    use x86_64::{structures::paging::Page, VirtAddr};
+
+    println!("Hello World{}", "!");
     sros::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let (level_4_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_table.start_address());
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    // as before
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-
     sros::hlt_loop();
 }
 
